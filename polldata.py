7a0b0583d02319292cc7f75d492d89e98dd189b8
bozzellaj <jbozzella@students.olin.edu>
2015-12-17 19:11:49 -0500

    added poll data

---
+++ polldata.py
@@ -0,0 +1,15 @@
+y1960 = [3, -1, 0, -1, 0, 0]		#Arranged Kennedy,Nixon
+y1976 = [0, 4, 1, -3, 2, 3]			#Arranged Carter, Ford
+y1980 = [5, -7, 4, 2]				#Arranged Reagan, Carter
+y1984 = [2, -1, -1, 2]				#Arranged Reagan, Mondale
+y1988 = [-2, 1, 1, -2]				#Arranged Bush, Dukakis
+y1992 = [-4, -1, 5, 1, -2, 0, -4, 2, 2]		#Arranged Clinton, Bush, Perot
+y1996 = [1, -2, 1, 6, -7, 3]		#Arranged Clinton, Dole, Perot
+y2000 = [4, -4, 5, -2, 2, -2]		#Arranged Bush, Gore
+y2004 = [-4, 5, -1, 1, 1, -2]		#Arranged Bush. Kerry
+y2008 = [1, -2, 1, -1, 0, 0]		#Arragned Obama, McCain
+y2012 = [0, 1, 1, 0, 0, 0]			#Arragned Obama, Romney
+
+Poll_data = y1960+y1976+y1980+y1984+y1988+y1992+y1996+y2000+y2004+y2008+y2012
+
+print Poll_data
